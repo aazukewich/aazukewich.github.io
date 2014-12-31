d3734127dde5e1f93a320954b7254937124648e5
jgzuke <jgzuke@gmail.com>
2014-12-31 01:50:06 -0500

    upload your pictures

---
+++ js/mysite.js
@@ -0,0 +1,35 @@
+$('#HomeLink').click(function()
+{
+	$('html, body').animate(
+	{
+		scrollTop: $('#Home').offset().top
+	}, 'slow');
+});
+$('#AboutLink').click(function()
+{
+	$('html, body').animate(
+	{
+		scrollTop: $('#About').offset().top
+	}, 'slow');
+});
+$('#PhotosLink').click(function()
+{
+	$('html, body').animate(
+	{
+		scrollTop: $('#Photos').offset().top
+	}, 'slow');
+});
+$('#ShowsLink').click(function()
+{
+	$('html, body').animate(
+	{
+		scrollTop: $('#Shows').offset().top
+	}, 'slow');
+});
+$('#ContactLink').click(function()
+{
+	$('html, body').animate(
+	{
+		scrollTop: $('#Contact').offset().top
+	}, 'slow');
+});
