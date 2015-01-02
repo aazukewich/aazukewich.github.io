0fb5f8f0edbfff4a59c45d0286c4162b8231745a
jgzuke <jgzuke@gmail.com>
2015-01-02 00:45:05 -0500

    background and stuff

--- js/mysite.js
+++ js/mysite.js
@@ -10,44 +10,63 @@
 		scrollTop: $('#Home').offset().top
 	}, 'slow');
 });*/
-var currentSong;
+var currentSong = 0;
 var songs = ['audio/song1.mp3', 'audio/song2.mp3', 'audio/song3.mp3'];
+var playingnow = 1;
 
 function playSong(songNum){
     currentSong=songNum;
     changeSong();
-};
-
+}
 //to use in other
 function prevSong(){
     if (currentSong > 0){
         currentSong -= 1;
         changeSong();
     };
-};
-
+}
 function nextSong(){
     if (currentSong < songs.length){
         currentSong += 1;
         changeSong();
     };
-};
+}
 function changeSong(){
     var newSong = songs[currentSong];
     var player = document.getElementById('audio');
     player.setAttribute('src', newSong);
     player.load();
-    player.play();
-};
-function playSong(){
+    play();
+}
+function togglePlay(){
+	if(playingnow == 1)
+	{
+		pause();
+	} else
+	{
+		play();
+	}
+}
+function play()
+{
+	playingnow = 1;
+    $('.playSong').hide();
+    $('.pauseSong').show();
     var player = document.getElementById('audio');
     player.play();
-};
-function pauseSong(){
+}
+function pause()
+{
+	playingnow = 0;
+    $('.playSong').show();
+    $('.pauseSong').hide();
     var player = document.getElementById('audio');
     player.pause();
-};
-
+}
+$(document).ready(function()
+{
+	$('.playSong').hide();
+});
 
 // scrolling with navbar
 $('#HomeLink').click(function()
